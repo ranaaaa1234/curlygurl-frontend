19e9bd75e87c004e9ed570563b66f3e630566880
ranaSafi <ranaa_safi@hotmail.com>
2025-09-27 15:09:52 +0200

    fixed styling for admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -22,12 +22,12 @@ const AdminOrders = () => {
 
   useEffect(() => {
     fetch("http://localhost:4000/admin/orders")
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         setOrders(data);
         setLoading(false);
       })
-      .catch(err => {
+      .catch((err) => {
         console.error(err);
         setLoading(false);
       });
@@ -37,19 +37,22 @@ const AdminOrders = () => {
 
   return (
     <div className="p-10 max-w-5xl mx-auto">
-      <h1 className="text-3xl font-bold mb-6">Admin Orders</h1>
-      {orders.map(order => (
-        <div key={order.id} className="border p-4 mb-4 rounded shadow">
-          <h2 className="font-bold">Order #{order.id}</h2>
-          <p>Date: {new Date(order.created_at).toLocaleString()}</p>
-          <p>Total: {order.total} kr</p>
+      <h1 className="text-3xl text-purple-900 font-bold mb-6">Orders</h1>
+      {orders.map((order) => (
+        <div
+          key={order.id}
+          className="border p-4 mb-4 rounded shadow hover:shadow-lg transition hover:border-purple-900"
+        >
+          <h2 className="font-bold text-purple-400">Order #{order.id}</h2>
+          <p>Date and time: {new Date(order.created_at).toLocaleString()}</p>
           <ul className="mt-2">
-            {order.items.map(item => (
+            {order.items.map((item) => (
               <li key={item.product_id}>
-                {item.name} x {item.quantity} - {item.price} kr
+               - {item.quantity} x {item.name}: {item.price} kr
               </li>
             ))}
           </ul>
+          <p className="mt-2 font-bold text-purple-400">Total: {order.total} kr</p>
         </div>
       ))}
     </div>
