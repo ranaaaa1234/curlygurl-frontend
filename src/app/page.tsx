83def2485ed94029c01c927721c0a7c7989f3eb6
ranaSafi <ranaa_safi@hotmail.com>
2025-11-04 12:35:33 +0100

    title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,6 @@ import "./globals.css";
 
 import ProductsPage from "./components/products/ProductsPage";
 
-export const metadata = {
-  title: "Curly Gurl",
-  description: "Explore our products",
-};
-
 export default function Home() {
   return (
     <div>
